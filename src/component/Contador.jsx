38901fd5d452f8a1cb8c5788e9bed64d30b11b70
Kentliel <kent.iluminati@gmail.com>
2023-07-19 13:27:59 +0200

    algo mas de tailwind

--- src/component/Contador.jsx
+++ src/component/Contador.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 const Contador = () => {
     const [number, setnumber] = useState(0)
+
     const Summation = () => {
         setnumber(number + 1);
     }
@@ -13,12 +14,12 @@ const Contador = () => {
     }
 
     return (
-        <div className='flex flex-col justify-center items-center'>
+        <div className='flex flex-col justify-center items-center gap-4'>
             <h1 className='font-bold text-2xl'>Contador por botones</h1>
             <h3 className='font-semibold text-base'>{number}</h3>
             <div className='flex gap-2 justify-center'>
-                <button className='border-2 px-2 py-1 bg-green-500 rounded' onClick={Summation}>+</button>
-                <button className='border-2 px-2 py-1 bg-red-500 rounded' onClick={Rest}>-</button>
+                <button className='border-2 px-2 py-1 bg-green-500 rounded' onClick={Summation}><span className='font-bold text-lg'>+</span> Incrementar</button>
+                <button className='border-2 px-2 py-1 bg-red-500 rounded' onClick={Rest}><span className='font-bold text-lg'>-</span> Disminuir</button>
             </div>
 
         </div>
