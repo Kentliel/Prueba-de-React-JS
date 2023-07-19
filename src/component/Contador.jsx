f7b4e417b5280d5356fc25c85be0feeef5b61fd4
Kentliel <kent.iluminati@gmail.com>
2023-07-19 13:23:57 +0200

    Se agrega algo de tailwind

--- src/component/Contador.jsx
+++ src/component/Contador.jsx
@@ -1,26 +1,28 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 const Contador = () => {
     const [number, setnumber] = useState(0)
     const Summation = () => {
         setnumber(number + 1);
     }
-    
+
     const Rest = () => {
         if (number != 0) {
             setnumber(number - 1)
         }
     }
 
-  return (
-    <div>
-        <h1>Contador por botones</h1>
-        <h3>{number}</h3>
-        <button onClick={Summation}>+</button> 
-        <button onClick={Rest}>-</button>
+    return (
+        <div className='flex flex-col justify-center items-center'>
+            <h1 className='font-bold text-2xl'>Contador por botones</h1>
+            <h3 className='font-semibold text-base'>{number}</h3>
+            <div className='flex gap-2 justify-center'>
+                <button className='border-2 px-2 py-1 bg-green-500 rounded' onClick={Summation}>+</button>
+                <button className='border-2 px-2 py-1 bg-red-500 rounded' onClick={Rest}>-</button>
+            </div>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Contador
